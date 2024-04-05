f2d777eee9237714786f14716e8c0d9fdf2c3afe
nivharosh <nivh1357@gmail.com>
2024-04-05 13:37:39 +0300

    on me

--- golbals.h
+++ golbals.h
@@ -1,2 +1,13 @@
 //*This is where we start*//
 //*Another one*//
+
+#define REG_COUNT 8
+#define OPCODE_COUNT 16
+#define MAX_LINE_LENGTH 81
+#define MAX_LABEL_LENGTH 31
+#define INSTRUCTIONS_COUNT 4
+#define MACRO_DECLARATION "mcr"
+#define END_MACRO_DECLARATION "endmcr"
+#define AS_FILE_FINISH ".as"
+#define AM_FILE_FINISH ".am"
+#define MAX_SYMBOL 4096
